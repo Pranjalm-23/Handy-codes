2cb331bfdd868129eead972d113438d0ab5b803f
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-03-16 11:34:31 +0530

    ichar correction

--- Recursion/get-keypad-combination.java
+++ Recursion/get-keypad-combination.java
@@ -25,7 +25,7 @@ public class Main {
     ArrayList<String> recResult = getKPC(restOfString);
     ArrayList<String> myResult = new ArrayList<>();
 
-    String codeForCh = codes[ch - 48]; // to convert to the real integer value from ascii (48 = 'a')
+    String codeForCh = codes[ch - 48]; // to convert to the real integer value from ascii (48 = '0')
     for (int i = 0; i < codeForCh.length(); i++) {
       char chCode = codeForCh.charAt(i);
       for (String rstr : recResult) {
