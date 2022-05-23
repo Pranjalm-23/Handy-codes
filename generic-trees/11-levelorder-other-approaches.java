ccad913ccee1e1ff5efc7377ad953d235af1ec88
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-05-23 20:31:52 +0530

    more gt-using-recursion

---
+++ generic-trees/11-levelorder-other-approaches.java
@@ -0,0 +1,72 @@
+// using two queues
+// done earlier
+
+// delimitor aaproach -- dummy node
+
+public static void levelOrderLinewise2(Node node) {
+  Queue < Node > mq = new ArrayDeque < > ( );
+
+  mq.add(node);
+  mq.add(new Node (-1));
+
+  while (mq.size() > 0) {
+    node = mq.remove();
+    if (node.data != -1) {
+      System.out.print(node.data + " ");
+
+      for (Node child : node.children) {
+        mq.add(child);
+      }
+    } else {
+      if (mq.size() > 0) {
+        mq.add(new Node(-1));
+        System.out.println();
+      }
+    }
+  }
+}
+
+
+// count approach -- size variable
+
+public static void levelOrderLinewise3(Node node) {
+  Queue < Node > mq = new ArrayDeque < > ( );
+
+  mq.add(node);
+
+  while (mq.size() > 0) {
+    int cicl = mq.size();
+    for (int i = 0; i < cicl; i++) {
+      node = mq.remove();
+      System.out.print(node.data + " ");
+
+      for (Node child : node.children) {
+        mq.add(child);
+      }
+    }
+    System.out.println();
+  }
+}
+
+// Using Pair Node
+
+public static void levelOrderLinewise4(Node node) {
+  Queue < Pair > mq = new ArrayDeque < > ( );
+
+  mq.add(new Pair(node, 1));
+
+  int level = 1;
+  while (mq.size() > 0) {
+    Pair p = mq.remove();
+    if (p.level > level) {
+      level = p.level;
+      System.out.println();
+    }
+
+    System.out.print(p.node.data + " ");
+    for (Node child : p.node.children) {
+      Pair cp = new Pair(child, p.level + 1);
+      mq.add(cp);
+    }
+  }
+}
